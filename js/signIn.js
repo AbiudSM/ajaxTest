417375895133950e271ad5941651e79e9e940683
Robertudo <sayamosfero@gmail.com>
2021-01-29 01:25:07 -0600

    Password validations

---
+++ js/signIn.js
@@ -0,0 +1,59 @@
+$(function(){
+
+	// Functions
+	verifyUser();
+	
+	$('#errorAlert').hide();
+
+	$('#signinForm').submit(function(event) {
+		event.preventDefault();
+		$('#errorAlert').hide();
+
+		// Password validation
+		if ($('#userPassword').val() != $('#userPasswordConfirm').val()) {
+			$('#textAlert').html('Passwords must match');
+			$('#errorAlert').show('fast');
+		}else{
+			// const userData = {
+			// 	name: $('#userName').val(),
+			// 	pass: $('#userPassword').val()
+			// }
+
+			// $.post('php/logIn.php', userData, function(response) {
+			// 	try{
+			// 	// If user is found
+			// 	let user = JSON.parse(response);
+			// 	window.location.href = "index.html";
+			// 	console.log(user);
+
+			// 	}catch{
+
+			// 		$('#textAlert').html(response);
+			// 		$('#errorAlert').show('fast');
+
+			// 	}
+
+			// });
+
+			// $('#loginForm').trigger("reset");
+
+			console.log('Password match');
+		}
+
+	});
+
+	// User login validations
+	function verifyUser(){
+		$.get('php/getUser.php', function(data) {
+			try{
+				var user = JSON.parse(data);
+				window.location.href = "index.html";
+			}
+			catch{
+				console.log('Inicia sesión');
+			}
+
+		});
+	}
+
+});
